f554b0ff764f7fd01f1d6f3ec5dc8698ef94f80d
David Martin <davidjmartin01@gmail.com>
2017-03-08 15:01:29 -0600

    api skeleton

--- server/api.js
+++ server/api.js
@@ -4,9 +4,11 @@ const db = require('APP/db')
 const api = module.exports = require('express').Router()
 
 api
-  .get('/heartbeat', (req, res) => res.send({ok: true,}))
   .use('/auth', require('./auth'))
   .use('/users', require('./users'))
+  .use('/recordings', require('./recordings'))
+  .use('/kits', require('./kits'))
+  .use('/samples', require('./samples'))
 
 // No routes matched? 404.
 api.use((req, res) => res.status(404).end())
