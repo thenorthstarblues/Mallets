5d5ad4effeaaed294c920563a12bba1bb4cb552f
David Martin <davidjmartin01@gmail.com>
2017-05-31 07:37:50 -0700

    pointd welcome component link to room menu

--- app/components/Welcome.jsx
+++ app/components/Welcome.jsx
@@ -1,15 +1,13 @@
-import React from 'react'
-import { Link } from 'react-router'
+import React from 'react';
+import { Link } from 'react-router';
 
-const Welcome = (props) => {
-  return (
+const Welcome = () => (
     <div className="open container is-fluid animated slideInUp">
       <h1 className="intro title">MALLETS</h1>
       <h3 className="subtitle is-5">
-        Let's play! You just need to <Link to="/signup">SIGN UP</Link>
+        Let's play! You just need to enter a <Link to="/menu">ROOM</Link>.
       </h3>
     </div>
-  )
-}
+  );
 
 export default Welcome;
