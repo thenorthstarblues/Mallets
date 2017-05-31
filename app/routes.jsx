34d131a70f2dba18218046c636411b474c22ce9e
David Martin <davidjmartin01@gmail.com>
2017-05-31 07:34:16 -0700

    removed signup and login from routes

--- app/routes.jsx
+++ app/routes.jsx
@@ -1,28 +1,23 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
-import Frame from './components/Frame'
-import Welcome from './components/Welcome'
-import Signup from './components/Signup'
-import Kit from './components/Kit'
-import Login from './components/Login'
-import Menu from './components/Menu'
-import NewRoom from './components/NewRoom'
-import JoinRoom from './components/JoinRoom'
-import Keyboards from './components/Keyboards'
-import WhoAmI from './components/WhoAmI'
+import Frame from './components/Frame';
+import Welcome from './components/Welcome';
+import Kit from './components/Kit';
+import Menu from './components/Menu';
+import NewRoom from './components/NewRoom';
+import JoinRoom from './components/JoinRoom';
+import Keyboards from './components/Keyboards';
 
 export default (
   <Route path="/" component={Frame}>
     <IndexRoute component={Welcome} />
     <Route path="/welcome" component={Welcome} />
-    <Route path="/signup" component={Signup} />
-    <Route path="/login" component={Login} />
     <Route path="/kit" component={Kit} />
     <Route path="/menu" component={Menu} />
     <Route path="/new-room" component={NewRoom} />
     <Route path="/join-room" component={JoinRoom} />
     <Route path="/keyboards" component={Keyboards} />
   </Route>
-)
+);
 
